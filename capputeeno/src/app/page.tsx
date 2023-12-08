4a6795b67c04e39dc6c5932c70cc81afcab40048
Pedro Galembeck <galembeckpedro@gmail.com>
2023-12-07 21:28:05 -0300

    feat: initial products fetch

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { FilterBar } from "@/components/catalogue/filters/filter-bar";
+import { ProductsList } from "@/components/catalogue/products/products-list";
+
+import styles from "./page.module.css";
+
 export default function Home() {
-  return;
+  const client = new QueryClient();
+
+  return (
+    <QueryClientProvider client={client}>
+      <main className={styles.main}>
+        <FilterBar />
+        <ProductsList />
+      </main>
+    </QueryClientProvider>
+  );
 }
---
+++ capputeeno/src/components/catalogue/products/products-list.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useProducts } from "@/hooks/useProducts";
+
+interface ProductsListProps {}
+
+export function ProductsList(props: ProductsListProps) {
+  const { data } = useProducts();
+
+  return <></>;
+}
