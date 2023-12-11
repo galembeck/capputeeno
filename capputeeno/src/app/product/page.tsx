0754499e45418d6be2c9b9fe11e53ea5fcdef0ee
Pedro Galembeck <galembeckpedro@gmail.com>
2023-12-11 12:42:28 -0300

    feat: add to cart button

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -120,6 +120,29 @@ export default function Product({
   searchParams: { id: string };
 }) {
   const { data } = useProduct(searchParams.id);
+
+  const handleAddToCart = () => {
+    let cartItems = localStorage.getItem("cart-items");
+    if (cartItems) {
+      let cartItemsArray = JSON.parse(cartItems);
+
+      let existingProductIndex = cartItemsArray.findIndex(
+        (item: { id: string }) => item.id === searchParams.id
+      );
+
+      if (existingProductIndex != -1) {
+        cartItemsArray[existingProductIndex].quantity += 1;
+      } else {
+        cartItemsArray.push({ ...data, quantity: 1, id: searchParams.id });
+      }
+
+      localStorage.setItem("cart-items", JSON.stringify(cartItemsArray));
+    } else {
+      const newCart = [{ ...data, quantity: 1, id: searchParams.id }];
+      localStorage.setItem("cart-items", JSON.stringify(newCart));
+    }
+  };
+
   return (
     <DefaultPageLayout>
       <Container>
@@ -142,7 +165,7 @@ export default function Product({
               </div>
             </ProductInfo>
 
-            <button>
+            <button onClick={handleAddToCart}>
               <ShoppingBagIcon />
               Adicionar ao carrinho
             </button>
